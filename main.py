c025fbf92f947d3bd80c43348d25ff99e9c9fc9e
Grant Miller-Francisco <gmillerf@hdrinc.com>
2026-02-12 17:38:51 -0800

    Test 4

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from nicegui import ui
 @ui.page('/')
 def index():
     ui.label('Hello NiceGUI World!')
-    ui.button('Click me!', on_click=lambda: ui.notify('Ahoy!! I have been clicked!'))
+    ui.button('Click me!', on_click=lambda: ui.notify('Ahoy!!! I have been clicked!'))
 
 if __name__ in {"__main__", "__mp_main__"}:
     ui.run(host='0.0.0.0', port=8080, reload=False)
