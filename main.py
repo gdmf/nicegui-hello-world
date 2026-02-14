65c08f2a86390b16ff7d254da7e46e2d0cc6db6e
Grant Miller-Francisco <gmillerf@hdrinc.com>
2026-02-13 16:21:41 -0800

    major commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from nicegui import ui
 def index():
     ui.label('Hello NiceGUI World (now using CI/CD...)!')
     ui.button('Click me!', on_click=lambda: ui.notify('I have been clicked!'))
+    ui.button("Don't Click me!", on_click=lambda: ui.notify('Oh no! I have been clicked!'))
 
 if __name__ in {"__main__", "__mp_main__"}:
     ui.run(host='0.0.0.0', port=8080, reload=False)
